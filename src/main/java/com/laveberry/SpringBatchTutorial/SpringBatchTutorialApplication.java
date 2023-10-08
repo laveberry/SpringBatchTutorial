93f0e5be1a71dd1a9ef9e48b92d57ae673178be0
laveberry <laveberry7@gmail.com>
2023-10-09 07:27:11 +0900

    Initial commit

---
+++ src/main/java/com/laveberry/SpringBatchTutorial/SpringBatchTutorialApplication.java
@@ -0,0 +1,14 @@
+package com.laveberry.SpringBatchTutorial;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchTutorialApplication.class, args);
+	}
+
+}
