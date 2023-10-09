7037205fd8cbd94239c8b2225222c9ea388954fa
laveberry <laveberry7@gmail.com>
2023-10-09 15:37:56 +0900

    (오류2)배치 작업의 기본, 파일 읽기와 쓰기

--- src/main/java/com/laveberry/SpringBatchTutorial/job/FileDataReadWrite/FileDataReadWriteConfig.java
+++ src/main/java/com/laveberry/SpringBatchTutorial/job/FileDataReadWrite/FileDataReadWriteConfig.java
@@ -2,6 +2,7 @@ package com.laveberry.SpringBatchTutorial.job.FileDataReadWrite;
 
 import com.laveberry.SpringBatchTutorial.job.FileDataReadWrite.dto.Player;
 import com.laveberry.SpringBatchTutorial.job.FileDataReadWrite.dto.PlayerFieldSetMapper;
+import com.laveberry.SpringBatchTutorial.job.FileDataReadWrite.dto.PlayerYears;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -10,10 +11,15 @@ import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -44,19 +50,55 @@ public class FileDataReadWriteConfig {
 
     @JobScope
     @Bean
-    public Step fileReadWriteStep(ItemReader playerItemReader) {
+    public Step fileReadWriteStep(ItemReader playerItemReader
+            , ItemProcessor playerItemProcessor
+            , ItemWriter playerItemWriter) {
         System.out.println("여기 들어옴???");
         return stepBuilderFactory.get("fileReadWriteStep")
-                .<Player, Player>chunk(5)
+                .<Player, PlayerYears>chunk(5)
                 .reader(playerItemReader)
-                .writer(new ItemWriter() {
-                    @Override
-                    public void write(List items) throws Exception {
-                        //출력
-                        items.forEach(System.out::println);
-                    }
-                })
+//                .writer(new ItemWriter() {
+//                    @Override
+//                    public void write(List items) throws Exception {
+//                        //출력
+//                        items.forEach(System.out::println);
+//                    }
+//                })
+                .processor(playerItemProcessor)
+                .writer(playerItemWriter)
                 .build();
+    }
+
+    @StepScope
+    @Bean
+    public FlatFileItemWriter<PlayerYears> playerItemWriter() {
+        BeanWrapperFieldExtractor<PlayerYears> fieldExtractor = new BeanWrapperFieldExtractor<>();
+        fieldExtractor.setNames(new String[]{"ID","lastName",  "position", "yearsExperience"});
+        fieldExtractor.afterPropertiesSet();
+
+        //기준 정의
+        DelimitedLineAggregator<PlayerYears> lineAggregator = new DelimitedLineAggregator<>();
+        lineAggregator.setDelimiter(","); //구분 기준
+        lineAggregator.setFieldExtractor(fieldExtractor); //추출필드
+
+        FileSystemResource outputResource = new FileSystemResource("player_output.txt");
+
+        return new FlatFileItemWriterBuilder<PlayerYears>()
+                .name("playerItemWriter")
+                .resource(outputResource)
+                .lineAggregator(lineAggregator)
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public ItemProcessor<Player, PlayerYears> playerItemProcessor() {
+        return new ItemProcessor<Player, PlayerYears>() {
+            @Override
+            public PlayerYears process(Player item) throws Exception {
+                return new PlayerYears(item);
+            }
+        };
     }
 
     // FlatFileItemReader : reporitory 아닌 file 에서 읽어올수 있는
