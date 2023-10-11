9c8d4dc4a3a94b3961810e0eac0bca27df8c1315
laveberry <laveberry7@gmail.com>
2023-10-12 05:47:25 +0900

    테스트코드 작성

---
+++ src/test/java/com/laveberry/SpringBatchTutorial/job/HelloWorld/HelloWorldJobConfigTest.java
@@ -0,0 +1,35 @@
+package com.laveberry.SpringBatchTutorial.job.HelloWorld;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test") //h2 기반 세팅
+@SpringBatchTest
+@SpringBootTest(classes = {SpringBatchTestConfig.class, HelloWorldJobConfig.class})
+class HelloWorldJobConfigTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Test
+    void success() throws Exception {
+        // when
+        JobExecution execution = jobLauncherTestUtils.launchJob();
+
+        // then
+        Assertions.assertEquals(execution.getExitStatus(), ExitStatus.COMPLETED);
+    }
+
+}
